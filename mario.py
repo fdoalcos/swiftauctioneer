a0124b0289c8aea3222ee89abded81720251b9b5
fdoalcos <fdoalcos@users.noreply.github.com>
2022-03-10 00:06:02 +0000

    automated commit by check50 [check50=True]

--- mario.py
+++ mario.py
@@ -1,9 +1,14 @@
 # TODO
 from cs50 import get_int
 
+while True:
+    
+
+
+
 n = get_int("Height: ")
 
-for row in range(n):
+for row in range(n + 1):
     for space in range(n - row):
         print(" ", end="")
     for column in range(row):
--- mario2.py
+++ mario2.py
@@ -1,9 +1,12 @@
 from cs50 import get_int
 
-n = get_int("Height: ")
+while True:
+    n = get_int("Height: ")
+    if n > 1 and n < 8:
+        break
 
 for row in range(n):
-    for space in range(n - row):
+    for space in range(n - row - 1):
         print(" ", end="")
     for column in range(row):
         print("#", end="")
