38eb20967cda5508fc9b5c63644385cade7169ba
fdoalcos <fdoalcos@users.noreply.github.com>
2022-03-17 03:16:19 +0000

    automated commit by check50 [check50=True]

--- 6.sql
+++ 6.sql
@@ -1,1 +1,1 @@
-SELECT AVG(rating) FROM ratings JOIN movies ON ratings.movie_id = movies.year WHERE year = 2012;
+SELECT AVG(rating) FROM ratings WHERE movie_id = (SELECT id FROM movies WHERE year = 2008);
