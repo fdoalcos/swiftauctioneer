b4da9c5b9436019f3daf6d2bd69874a45092b5bc
fdoalcos <fdoalcos@users.noreply.github.com>
2022-05-24 03:29:19 +0000

    automated commit by check50 [check50=True]

--- readability.py
+++ readability.py
@@ -2,25 +2,30 @@
 from cs50 import get_string
 
 text = get_string("Text: ")
-words = letters = sentences = i = 0
+words = 0
+letters = 0 
+sentences = 0
 length = len(text)
 
-# counting letters
-if text[i].isalpha():
-    letters += 1
 
-# counting words
-if (i == 0 and text[i] != " ") or (i != length and text[i] == " " and text[i + 1] != " "):
-    words += 1
 
-#counting letters
-if text[i] == "." or text[i] == "?" or text[i] == "," or text[i] == "!":
-    sentences += 1
+for i in range(length):
+    # counting letters
+    if text[i].isalpha():
+        letters += 1
+    
+    # counting words
+    if (i == 0 and text[i] != " ") or (i != length and text[i] == " " and text[i + 1] != " "):
+        words += 1
+    
+    #counting letters
+    if text[i] == "." or text[i] == "?" or text[i] == "," or text[i] == "!":
+        sentences += 1
 
 L = (letters / words) * 100
 S = (sentences / words) * 100
 
-index = round(0.0588 * L - 0.296 * S -15.8)
+index = int(round(0.0588 * L - 0.296 * S -15.8))
 
 if index > 16:
     print("Grade 16+")
