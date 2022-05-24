2e66bd0a5dc99cae7aadd2563dbae22fcd49da46
fdoalcos <fdoalcos@users.noreply.github.com>
2022-05-24 19:29:33 +0000

    automated commit by check50 [check50=True]

--- 4.sql
+++ 4.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies, ratings WHERE movies.id = ratings.movie_id AND rating = 10.0;
+SELECT COUNT(title) FROM movies, ratings WHERE movies.id = ratings.movie_id AND rating = 10.0;
