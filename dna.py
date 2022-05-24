676ef5812babb56793dfe4581fc17f6ee444c77b
fdoalcos <fdoalcos@users.noreply.github.com>
2022-05-24 18:33:28 +0000

    automated commit by check50 [check50=True]

--- dna.py
+++ dna.py
@@ -6,41 +6,41 @@ def main():
 
     # TODO: Check for command-line usage
     if len(sys.argv) != 3:
-        print("Usage: python dna.py data.csv sequence.txt")
-        sys.exit(1)
+        print("Incorrect # of inputs")
+        exit()
+
     # TODO: Read database file into a variable
-    database = sys.argv[1]
-    sequence = sys.argv[2]
-
-
-    with open(database, "r") as file:
-        reader = csv.reader(file)
-        memory = list(reader)
+    with open(sys.argv[1]) as e:
+        reader = csv.DictReader(e)
+        database = list(reader)
 
     # TODO: Read DNA sequence file into a variable
-    with open(sequence, "r") as dna:
-        dnareader = dna.read()
+    with open(sys.argv[2]) as f:
+        sequence = f.read()
 
     # TODO: Find longest match of each STR in DNA sequence
-    matches = []
-    for i in range(1, len(memory[0])):
-        matches.append(longest_match(dnareader, memory[0][i]))
+    matches = {}
+
+    #This results in "name" : 0
+    for i in database[0]:
+        matches[i] = (longest_match(sequence, i))
 
     # TODO: Check database for matching profiles
-    suspect = "No Match"
-    suspect_counter = 0
+    # Counter starts at 1, since there won't be a 'name' match
+    suspect = 'No Match'
+    suspect_counter = 1
 
-    for i in range(1, len(memory)):
-        for j in range(len(matches)):
-
-            if matches[j] == int(memory[i][j + 1]):
-                   suspect_counter += 1
-
+    for i in range(len(database)):
+        for j in matches:
+            #Matches values are ints, need to cast them to strings for comparison
+            if str(matches[j]) == database[i][j]:
+                suspect_counter += 1
         if suspect_counter == len(matches):
-            suspect = database[i][0]
+            #We've got the suspect!  No need to continue
+            suspect = database[i]['name']
             break
         else:
-            suspect_counter = 0
+            suspect_counter = 1
 
     print(suspect)
 
